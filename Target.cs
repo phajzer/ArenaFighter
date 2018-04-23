d3dcd727b8b88207fd2580db9eb93a626fe34297
Patryk <hajzer.patryk@gmail.com>
2018-04-23 16:02:27 +0200

    Add files via upload

---
+++ Target.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    public float health = 50f;
+    Animator anim;
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if (health <= 0f)
+        {
+            Die();
+        }
+    }
+    void Die()
+    {
+        anim.Play("Death");
+        Destroy(gameObject);
+    }
+}
---
+++ gun.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+
+public class gun : MonoBehaviour {
+
+    public float damage = 10f;
+    public float range = 100f;
+    public float impactForce = 40f;
+    public float fireRate = 15f;
+
+    public Camera fpsCam;
+    public ParticleSystem muzzleFlash;
+    public GameObject impact;
+
+    private float nextTimeToFire = 0f;
+
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            Shoot();
+        }
+	}
+
+    void Shoot()
+    {
+        muzzleFlash.Play();
+
+        RaycastHit hit;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Target target = hit.transform.GetComponent<Target>();
+            if( target !=null)
+            {
+                target.TakeDamage(damage);
+            }
+
+            if (hit.rigidbody != null)
+            {
+                hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            GameObject impactGo = Instantiate(impact, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGo, 2f);
+        }
+    }
+}
