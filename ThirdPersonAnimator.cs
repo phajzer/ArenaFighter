e744997cc2c4918a0e96b6a23740be7929a72ed5
Patryk <hajzer.patryk@gmail.com>
2018-04-23 16:01:35 +0200

    Add files via upload

---
+++ ThirdPersonAnimator.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Invector.CharacterController
+{
+    public abstract class vThirdPersonAnimator : vThirdPersonMotor
+    {
+        public virtual void UpdateAnimator()
+        {
+            if (animator == null || !animator.enabled) return;
+
+            animator.SetBool("IsStrafing", isStrafing);
+            animator.SetBool("IsGrounded", isGrounded);
+            animator.SetFloat("GroundDistance", groundDistance);
+
+            if (!isGrounded)
+                animator.SetFloat("VerticalVelocity", verticalVelocity);
+
+            if (isStrafing)
+            {
+                
+                animator.SetFloat("InputHorizontal", direction, 0.1f, Time.deltaTime);
+            }
+
+            
+            animator.SetFloat("InputVertical", speed, 0.1f, Time.deltaTime);
+        }
+
+        public void OnAnimatorMove()
+        {
+            
+            if (isGrounded)
+            {
+                transform.rotation = animator.rootRotation;
+
+                var speedDir = Mathf.Abs(direction) + Mathf.Abs(speed);
+                speedDir = Mathf.Clamp(speedDir, 0, 1);
+                var strafeSpeed = (isSprinting ? 1.5f : 1f) * Mathf.Clamp(speedDir, 0f, 1f);
+                
+                
+                if (isStrafing)
+                {
+                    if (strafeSpeed <= 0.5f)
+                        ControlSpeed(strafeWalkSpeed);
+                    else if (strafeSpeed > 0.5f && strafeSpeed <= 1f)
+                        ControlSpeed(strafeRunningSpeed);
+                    else
+                        ControlSpeed(strafeSprintSpeed);
+                }
+                else if (!isStrafing)
+                {
+                                 
+                    if (speed <= 0.5f)
+                        ControlSpeed(freeWalkSpeed);
+                    else if (speed > 0.5 && speed <= 1f)
+                        ControlSpeed(freeRunningSpeed);
+                    else
+                        ControlSpeed(freeSprintSpeed);
+                }
+            }
+        }
+    }
+}
---
+++ ThirdPersonInput.cs
@@ -0,0 +1,175 @@
+﻿using UnityEngine;
+#if UNITY_5_3_OR_NEWER
+using UnityEngine.SceneManagement;
+#endif
+
+namespace Invector.CharacterController
+{
+    public class vThirdPersonInput : MonoBehaviour
+    {
+        #region variables
+
+        [Header("Default Inputs")]
+        public string horizontalInput = "Horizontal";
+        public string verticallInput = "Vertical";
+        public KeyCode jumpInput = KeyCode.Space;
+        public KeyCode strafeInput = KeyCode.Tab;
+        public KeyCode sprintInput = KeyCode.LeftShift;
+
+        [Header("Camera Settings")]
+        public string rotateCameraXInput ="Mouse X";
+        public string rotateCameraYInput = "Mouse Y";
+
+        protected vThirdPersonCamera tpCamera;                     
+        [HideInInspector]
+        public string customCameraState;                            
+        [HideInInspector]
+        public string customlookAtPoint;                           
+        [HideInInspector]
+        public bool changeCameraState;                          
+        [HideInInspector]
+        public bool smoothCameraState;                      
+        [HideInInspector]
+        public bool keepDirection;                          
+
+        protected vThirdPersonController cc;                               
+
+        #endregion
+
+        protected virtual void Start()
+        {
+            CharacterInit();
+        }
+
+        protected virtual void CharacterInit()
+        {
+            cc = GetComponent<vThirdPersonController>();
+            if (cc != null)
+                cc.Init();
+
+            tpCamera = FindObjectOfType<vThirdPersonCamera>();
+            if (tpCamera) tpCamera.SetMainTarget(this.transform);
+
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+
+        protected virtual void LateUpdate()
+        {
+            if (cc == null) return;             	    
+            InputHandle();                      
+            UpdateCameraStates();               
+        }
+
+        protected virtual void FixedUpdate()
+        {
+            cc.AirControl();
+            CameraInput();
+        }
+
+        protected virtual void Update()
+        {
+            cc.UpdateMotor();                              
+            cc.UpdateAnimator();                		               
+        }
+
+        protected virtual void InputHandle()
+        {
+            ExitGameInput();
+            CameraInput();
+
+            if (!cc.lockMovement)
+            {
+                MoveCharacter();
+                SprintInput();
+                StrafeInput();
+                JumpInput();
+            }
+        }
+
+        #region Basic Locomotion Inputs      
+
+        protected virtual void MoveCharacter()
+        {            
+            cc.input.x = Input.GetAxis(horizontalInput);
+            cc.input.y = Input.GetAxis(verticallInput);
+        }
+
+        protected virtual void StrafeInput()
+        {
+            if (Input.GetKeyDown(strafeInput))
+                cc.Strafe();
+        }
+
+        protected virtual void SprintInput()
+        {
+            if (Input.GetKeyDown(sprintInput))
+                cc.Sprint(true);
+            else if(Input.GetKeyUp(sprintInput))
+                cc.Sprint(false);
+        }
+
+        protected virtual void JumpInput()
+        {
+            if (Input.GetKeyDown(jumpInput))
+                cc.Jump();
+        }
+
+        protected virtual void ExitGameInput()
+        {
+            
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                if (!Cursor.visible)
+                    Cursor.visible = true;
+                else
+                    Application.Quit();
+            }
+        }
+
+        #endregion
+
+        #region Camera Methods
+
+        protected virtual void CameraInput()
+        {
+            if (tpCamera == null)
+                return;
+            var Y = Input.GetAxis(rotateCameraYInput);
+            var X = Input.GetAxis(rotateCameraXInput);
+
+            tpCamera.RotateCamera(X, Y);
+
+            
+            if (!keepDirection)
+                cc.UpdateTargetDirection(tpCamera != null ? tpCamera.transform : null);
+                   
+            RotateWithCamera(tpCamera != null ? tpCamera.transform : null);            
+        }
+
+        protected virtual void UpdateCameraStates()
+        {
+              if (tpCamera == null)
+            {
+                tpCamera = FindObjectOfType<vThirdPersonCamera>();
+                if (tpCamera == null)
+                    return;
+                if (tpCamera)
+                {
+                    tpCamera.SetMainTarget(this.transform);
+                    tpCamera.Init();
+                }
+            }            
+        }
+
+        protected virtual void RotateWithCamera(Transform cameraTransform)
+        {
+            if (cc.isStrafing && !cc.lockMovement && !cc.lockMovement)
+            {                
+                cc.RotateWithAnotherTransform(cameraTransform);                
+            }
+        }
+
+        #endregion     
+    }
+}
